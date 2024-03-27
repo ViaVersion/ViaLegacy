9ed5575a29bddef6e2bdd1a117dfdd79ba4ae11a
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2024-03-27 02:03:18 +0100

    Handle out of bounds data values for stone

--- src/main/java/net/raphimc/vialegacy/protocols/release/protocol1_8to1_7_6_10/storage/ChunkTracker.java
+++ src/main/java/net/raphimc/vialegacy/protocols/release/protocol1_8to1_7_6_10/storage/ChunkTracker.java
@@ -34,6 +34,9 @@ public class ChunkTracker extends AbstractChunkTracker {
         for (int i = 1; i < 16; i++) { // air
             this.registerReplacement(new IdAndData(0, i), new IdAndData(0, 0));
         }
+        for (int i = 1; i < 7; i++) { // stone variants
+            this.registerReplacement(new IdAndData(BlockList1_6.stone.blockID, i), new IdAndData(BlockList1_6.stone.blockID, 0));
+        }
         this.registerReplacement(new IdAndData(BlockList1_6.chest.blockID, 0), new IdAndData(BlockList1_6.chest.blockID, 3));
     }
 
