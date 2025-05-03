fa6cc65457fbfb0865d80a40d7398971cae0eb99
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2025-05-04 00:38:48 +0200

    Split monolithic gradle files into modular gradle snippets

--- src/main/java/net/raphimc/vialegacy/ViaLegacy.java
+++ src/main/java/net/raphimc/vialegacy/ViaLegacy.java
@@ -23,7 +23,7 @@ import net.raphimc.vialegacy.platform.ViaLegacyPlatform;
 public class ViaLegacy {
 
     public static final String VERSION = "${version}";
-    public static final String IMPL_VERSION = "${impl_version}";
+    public static final String IMPL_VERSION = "git-ViaLegacy-${version}:${commit_hash}";
 
     private static ViaLegacyPlatform platform;
     private static ViaLegacyConfig config;
